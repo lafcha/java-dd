60a8f4d50693b71403b0267e5be1086f011c26f9
lafcha <charlotte.bronsard@gmail.com>
2022-05-16 09:14:50 +0200

    Only one weapon or spell functionnality ok

--- src/com/equipment/spells/Spell.java
+++ src/com/equipment/spells/Spell.java
@@ -22,15 +22,35 @@ public abstract class Spell extends Equipment {
      * @return a string indicating if the hero can have a spell & and how many attack points he gains.
      */
     public String openSurprise(Hero hero) {
+
         if (hero instanceof Wizard) {
-            if (hero.getForce() + this.getPower() > hero.getMaxAttackForce()) {
-                hero.setForce(hero.getMaxAttackForce());
+            if (hero.getWeapon() == null) {
+                if (hero.getForce() + this.getPower() > hero.getMaxAttackForce()) {
+                    hero.setForce(hero.getMaxAttackForce());
+                    hero.setWeapon(this);
+                    return "Vous avez ramassé un sort de type " + this.getName() + ". Il vous apporte " + (hero.getMaxAttackForce() - this.getPower()) + " points d'attaque.";
+                } else {
+                    hero.setForce(hero.getForce() + this.getPower());
+                    hero.setWeapon(this);
+                    return "Vous avez ramassé un sort de type " + this.getName() + ". Il apporte " + this.getPower() + " points d'attaque.";
+                }
+            } else if (hero.getWeapon().getPower() > this.getPower()) {
+                System.out.println("Vous récupérez un sort plus puissant que la votre et vous échangez donc le sort que vous aviez par celui que vous avez ramassé.");
+                if (((hero.getForce() - hero.getWeapon().getPower()) + this.getPower()) > hero.getMaxAttackForce()) {
+                    hero.setForce(hero.getMaxAttackForce());
+                    hero.setWeapon(this);
+                    return "Vous avez ramassé un sort de type " + this.getName() + ". Il vous apporte " + (hero.getMaxAttackForce() - this.getPower()) + " points d'attaque.";
+                } else {
+                    hero.setForce((hero.getForce() - hero.getWeapon().getPower())+ this.getPower());
+                    hero.setWeapon(this);
+                    return "Vous avez ramassé un sort de type " + this.getName() + ". Il vous apporte " + this.getPower() + " points d'attaque.";
+                }
+
             } else {
-                hero.setForce(hero.getForce() + this.getPower());
+                return "La case contient un sort de moins bonne qualité que celui que vous avez actuellement. Vous ne le ramassez donc pas.";
             }
-            return "Vous avez ramassé un sort de type " + this.getName() + ". Elle vous apporte " + this.getPower() + " points d'attaque.";
         } else {
-            return "Vous avez ramassé un sort de type " + this.getName() + " mais vous êtes un " + hero.getClass().getSimpleName() + " et vous ne pouvez pas l'utiliser. Vous ne bénéficiez pas de points d'attaque supplémentaires.";
+            return "Vous avez ramassé équipement de type " + this.getName() + " mais vous êtes un " + hero.getClass().getSimpleName() + " et vous ne pouvez pas l'utiliser. Vous ne bénéficiez pas de points d'attaque supplémentaires.";
         }
     }
 }
